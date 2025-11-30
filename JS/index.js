8029576db911f492d20a6a736136e9a84e5f93b6
mdsabbir112 <anikmullah112@gmail.com>
2025-11-30 08:10:07 +0600

    added user experience bar

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,16 @@
-//Fetch the Api
+// Create a function for toggle the specific button
 
+function RemoveActiveClass () {
+    // console.log('please remove active class');
+    const activeButtons = document.getElementsByClassName("active");
+
+    for(let btn of activeButtons){
+        btn.classList.remove("active");
+    }
+
+}
+
+//Fetch the Api
 function mainCategories (){
     //Api Request 
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -14,7 +25,20 @@ function mainCategories (){
 function loadCategoryVideos(id){
     // console.log(id);
     const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}` ;
-    console.log(url);
+    // console.log(url);
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+       
+        const clickButton = document.getElementById(`btn-${id}`);
+        RemoveActiveClass();
+        clickButton.classList.add("active");
+        console.log(clickButton);
+
+        loadVideo(data.category)
+    })
 }
 
 // Add a function from there 
@@ -37,7 +61,7 @@ function loadCategories (categories){
     //create an Elements
     let CategoriesDiv = document.createElement("div");
     CategoriesDiv.innerHTML = `
-         <button onclick = loadCategoryVideos(${Cat.category_id}) class="btn btn-soft hover:bg-[#FF1F3D] hover:text-white text-lg font-semibold">${Cat.category}</button>
+         <button id="btn-${Cat.category_id}" onclick = loadCategoryVideos(${Cat.category_id}) class="btn btn-soft hover:bg-[#FF1F3D] hover:text-white text-lg font-semibold">${Cat.category}</button>
     `
     //Append the child variable 
     CategorieData.append(CategoriesDiv);
@@ -46,11 +70,16 @@ function loadCategories (categories){
 
 // Load the Video card for show the Video
 
-async function mainVideo(){
-    const response = await fetch("https://openapi.programming-hero.com/api/phero-tube/videos");
+ function mainVideo(){
+     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
 
-    let data = await response.json();
-    loadVideo(data.videos);
+    .then(response => response.json())
+    .then(data => {
+        RemoveActiveClass();
+        document.getElementById("btn-all").classList.add("active");
+        loadVideo(data.videos);
+    })
+    
 }
 
 
@@ -59,10 +88,27 @@ function loadVideo (videos) {
 
     const displyVideo = document.getElementById("showVideo") ;
 
+    displyVideo.innerHTML = "";
+    
+// check for empty array object based on id 
+     if(videos.length == 0) {
+            displyVideo.innerHTML = `
+             <div class="py-28 col-span-full flex justify-center items-center flex-col ">
+            <img src="assets/Icon.png" alt="icons">
+            <h2 class=" mt-8 text-center font-bold text-3xl text-[#171717]">Oops!! Sorry, There is no content here</h2>
+        </div>
+            `;
+
+            displyVideo.append(div);
+            
+            return; // that means nicher kono code are excecute hobe ne 
+        }
+
     // USe FOR each for loop the Array
     videos.forEach(function(video){
         // console.log(video);
         const div = document.createElement("div");
+
 
         /**
          * {category_id: '1001', 
@@ -119,8 +165,3 @@ function loadVideo (videos) {
 
 mainCategories()
 // mainVideo()
-
-
-// fetch (" https://openapi.programming-hero.com/api/phero-tube/category/1001")
-// .then (res => res.json())
-// .then (data => console.log(data))
